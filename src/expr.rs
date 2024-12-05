bea14cad84591fd09cd5ac4b3a8378dbee24a372
Mehmet Yalcinkaya <mehmet.yalcinkaya@novoenergy.se>
2024-12-05 23:08:03 +0100

    test pretty print logic

--- src/expr.rs
+++ src/expr.rs
@@ -1,4 +1,4 @@
-use crate::scanner::Token;
+use crate::scanner::{ Token, TokenType };
 
 pub enum LiteralValue {
     Number(f32),
@@ -42,9 +42,9 @@ impl Expr {
     pub fn to_string(&self) -> String {
         match self {
             Expr::Binary { left, operator, right } =>
-                format!("{} {} {}", operator.lexeme, left.to_string(), right.to_string()),
+                format!("({} {} {})", operator.lexeme, left.to_string(), right.to_string()),
             Expr::Grouping { expression } => format!("(group {})", (*expression).to_string()),
-            Expr::Literal { value } => format!("({})", value.to_string()),
+            Expr::Literal { value } => format!("{}", value.to_string()),
             Expr::Unary { operator, right } => {
                 let operator_str = operator.lexeme.clone();
                 let right_str = (*right).to_string();
@@ -55,5 +55,37 @@ impl Expr {
 
     pub fn print(&self) {
         println!("{}", self.to_string());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::Expr::*;
+    use super::LiteralValue::*;
+
+    #[test]
+    fn pretty_print_ast() {
+        let minus_token = Token {
+            token_type: TokenType::Minus,
+            lexeme: "-".to_string(),
+            literal: None,
+            line_number: 0,
+        };
+        let onetwothree = Literal { value: Number(123.0) };
+        let group = Grouping { expression: Box::from(Literal { value: Number(45.67) }) };
+        let multi = Token {
+            token_type: TokenType::Star,
+            lexeme: "*".to_string(),
+            literal: None,
+            line_number: 0,
+        };
+        let ast = Binary {
+            left: Box::from(Unary { operator: minus_token, right: Box::from(onetwothree) }),
+            operator: multi,
+            right: Box::from(group),
+        };
+        let result = ast.to_string();
+        assert_eq!(result, "(* (- 123) (group 45.67))");
     }
 }
